699fc1f14acd1a947ca29c7a5ef542286ac3ae63
hayate <suzuki.hayate.20b@shizuoka.ac.jp>
2022-11-04 11:23:16 +0900

    add fetch

--- pages/sleep_twice.tsx
+++ pages/sleep_twice.tsx
@@ -1,20 +1,41 @@
 import type { NextPage } from "next";
-import * as Obniz from "obniz";
-const Sleep_twice: NextPage = () => {
-  // const OBNIZ_ID = "2061-9919";
-  // const obniz = new Obniz(OBNIZ_ID);
-  // obniz.onconnect = async function () {
-  //   const servo = obniz.wired("ServoMotor", { gnd: 0, vcc: 1, signal: 2 });
+// import Sound from './決定、ボタン押下/4';
 
-  //   servo.angle(90.0);
-  // };
+export async function getServerSideProps() {
+  //   try {
+  await fetch(
+    "https://tqscqxulxh.execute-api.ap-northeast-1.amazonaws.com/default/obniz_slime"
+  );
+
+  return { props: {} };
+
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+}
+
+const Sleep_twice: NextPage = () => {
   return (
-    <div className="container border-4 border-whit mx-auto  rounded relative h-screen w-screen flex justify-center items-cente">
-      <h1 className="text-white text-7xl h-screen w-screen flex justify-center items-center font-medium relative mx-auto">
-        勇者を起こせなかった
-      </h1>
+    <div className="flex-flow:column justify-content:space-around align-items: center items-center">
+      <div className="container border-4 border-whit mx-auto  rounded relative items-center ">
+        <h1 className="text-white text-7xl  font-medium relative mx-auto ">
+          勇者を起こせなかった
+        </h1>
+        <h2>
+          <img
+            className="mt-6 rounded-lg shadow-xl object-contain items-center"
+            width={300}
+            height={300}
+            src="slime/スライムアイコン.png"
+          ></img>
+        </h2>
+        <h3>
+          <button className="bg-white hover:bg-white text-black rounded px-4 py-2 bottom-7">
+            ホームに戻る
+          </button>
+        </h3>
+      </div>
     </div>
   );
 };
-
 export default Sleep_twice;
