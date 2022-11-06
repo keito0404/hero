a06e52f1b7c9411a6281bc5214ccab1b99430b3b
keito <tetorapot44@gmail.com>
2022-11-07 04:59:34 +0900

    メインのページ

---
+++ pages/App.tsx
@@ -0,0 +1,20 @@
+import Clocks from '../components/Clocks'
+
+function App() {
+  return (
+    <div>
+      <Clocks />
+    </div>
+  )
+}
+
+// const SBackGround = styled.div`
+//   background-color: #333333;
+//   height: 100vh;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+// `
+
+export default App
