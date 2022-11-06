55144673cfb5838773ecd529d63fc39f6e06a290
keito <tetorapot44@gmail.com>
2022-11-07 05:00:06 +0900

    パッケージの更新

--- next.config.js
+++ next.config.js
@@ -3,5 +3,25 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
-
 module.exports = nextConfig
+
+// const withCSS = require('@zeit/next-css');
+// const compose = require('next-compose');
+// cssConfig = {};
+// module.exports = compose([
+//   [withCSS, cssConfig],
+//   {
+//     /** @type {import('next').NextConfig} */
+//     reactStrictMode: true,
+//   swcMinify: true,
+//     webpack(config, options) {
+//       config.module.rules.push({
+//         test: /\.mp3$/,
+//         use: {
+//           loader: 'file-loader',
+//         },
+//       });
+//       return config;
+//     },
+//   },
+// ]);
