760102881f6b268782c780fd256dc98e8eceac3c
yukikata <swim4000yuki@gmail.com>
2022-11-10 02:49:42 +0900

    first commit

---
+++ components/DisplayHp.tsx
@@ -0,0 +1,32 @@
+type Props = {
+  hp: number;
+};
+
+const DisplayHp = (props: Props) => {
+  let hpBar = "w-full";
+  const hp = props.hp;
+
+  if (hp == 0) {
+    hpBar = "hidden"; //何かあったら黒にする
+  } else if (hp == 20) {
+    hpBar = "w-1/5";
+  } else if (hp == 40) {
+    hpBar = "w-2/5";
+  } else if (hp == 60) {
+    hpBar = "w-3/5";
+  } else if (hp == 80) {
+    hpBar = "w-4/5";
+  } else if (hp == 100) {
+    hpBar = "w-full";
+  }
+
+  return (
+    <div className="flex justify-center">
+      <div className="p-3 w-4/5 bg-black flex px-3.5 border-2 border-white">
+        <div className={`p-3 ${hpBar} bg-white transition-all`}></div>
+      </div>
+    </div>
+  );
+};
+
+export default DisplayHp;
---
+++ pages/sleep.tsx
@@ -0,0 +1,92 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import DisplayHp from "../components/DisplayHp";
+import EasyMode from "../components/EasyMode";
+import NormalMode from "../components/NormalMode";
+import HardMode from "../components/HardMode";
+
+export default function Sleep() {
+  const router = useRouter();
+  const [hp, setHp] = useState(100);
+  const [incantation, setIncantation] = useState("");
+  const [randomNum, setRandomNum] = useState(0);
+  const [selectedIncation, setSelectedIncatation] = useState("");
+  const [attackResult, setAttackResult] = useState("");
+  const [selectedWord, setSelectedWord] = useState(""); //setSelectedWordは初期値空
+  const incantations = ["A", "B", "C", "D", "E"];
+  const difficultyLevel = ["easy", "normal", "hard"];
+  useEffect(() => {
+    shuffleIncations(incantations);
+    shuffleLevels(difficultyLevel);
+  }, []); //一番初めに実行される
+
+  const shuffleIncations = (vals: any) => {
+    let castIncation = Math.floor(Math.random() * vals.length);
+    setIncantation(incantations[castIncation]);
+  };
+
+  const shuffleLevels = (vals: any) => {
+    let selectLevel = Math.floor(Math.random() * vals.length);
+    setRandomNum(selectLevel);
+  };
+
+  const onChangeIncatation = (e: ChangeEvent<HTMLSelectElement>) =>
+    setSelectedIncatation(e.target.value);
+  const inputIncatation = (e: ChangeEvent<HTMLInputElement>) =>
+    setSelectedWord(e.target.value);
+
+  const attack = () => {
+    if (difficultyLevel[randomNum] == "easy") {
+      if (selectedIncation == incantation) {
+        setHp(hp - 20);
+        shuffleIncations(incantations);
+        setAttackResult("攻撃が成功した");
+      } else {
+        setAttackResult("攻撃が失敗した");
+      }
+    } else if (difficultyLevel[randomNum] == "normal") {
+      if (selectedWord == incantation) {
+        setHp(hp - 20);
+        shuffleIncations(incantations);
+        setAttackResult("攻撃が成功した");
+      } else {
+        setAttackResult("攻撃が失敗した");
+      }
+    } else if (difficultyLevel[randomNum] == "hard") {
+    }
+  };
+
+  const selectedLevel = (randomNum: number) => {
+    if (difficultyLevel[randomNum] == "easy") {
+      return <EasyMode onChangeIncatation={onChangeIncatation} />;
+    } else if (difficultyLevel[randomNum] == "normal") {
+      // TODO：１.onChangeIncationのように、inputIncatation関数を作る(ちなみに引数の型は
+      //          (e: ChangeEvent<HTMLInputElement>) )
+      //       2. useStateを利用して、inputIncatation変数を作る
+      //       3. NormalMode.tsxコンポーネントの中でpropsとしてinputIncatation関数を受け取る
+      //       4. これでノーマルモードは完成や！！（Next, ハードモード）
+      return <NormalMode inputIncatation={inputIncatation} />;
+    } else if (difficultyLevel[randomNum] == "hard") {
+      return <HardMode />;
+    }
+  };
+
+  return (
+    <div className="text-white">
+      <div>
+        {/* <audio controls autoplay src="./Levelup.mp3"></audio> */}
+        <p>残りHP：{hp}</p>
+        <p>相手にダメージを与える ：{incantation}</p>
+        {selectedLevel(randomNum)}
+        <button
+          className="bg-gray-600 hover:bg-gray-500 text-white rounded px-4 py-2"
+          onClick={attack}
+        >
+          覚醒
+        </button>
+        <p>{attackResult}</p>
+      </div>
+      <DisplayHp hp={hp} />
+    </div>
+  );
+}
