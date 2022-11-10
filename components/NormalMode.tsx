ed06349db33e0e16bb9faa0a1296e750632f0099
yukikata <swim4000yuki@gmail.com>
2022-11-10 21:14:25 +0900

    fix: 文字色黒にする

--- components/NormalMode.tsx
+++ components/NormalMode.tsx
@@ -4,12 +4,14 @@ const NormalMode = (props: any) => {
       <p>ノーマルモード</p>
       <label className="block">
         <span>呪文を入力</span>
-        <input
-          type="text"
-          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-          placeholder=""
-          onChange={props.inputIncatation}
-        />
+        <div className="text-black">
+          <input
+            type="text"
+            className="mt-1 block w-80 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            placeholder=""
+            onChange={props.inputIncatation}
+          />
+        </div>
       </label>
     </div>
   );
