b40cbcd250d83edf8efae7a4450491e11a882f2e
yukikata <swim4000yuki@gmail.com>
2022-11-10 21:13:57 +0900

    fix:選択肢を真ん中に寄せる

--- components/EasyMode.tsx
+++ components/EasyMode.tsx
@@ -2,12 +2,12 @@ const EasyMode = (props: any) => {
   return (
     <div>
       <form>
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
+        <label className="block mb-2 text-sm font-medium text-white dark:text-gray-400">
           呪文を選択する：
         </label>
         <select
           id="countries"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           onChange={props.onChangeIncatation}
         >
           <option value="A">A</option>
